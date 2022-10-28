f96df4f4c37d8ec643ccd6f7cd0e4e01154759a3
rockyraajesh <93480340+rockyraajesh@users.noreply.github.com>
2022-10-28 21:57:19 +0530

    Add files via upload

---
+++ hacktoberfest 2022/Sql/sorting.sql
@@ -0,0 +1,6 @@
+USE sql_hr;
+SELECT * FROM employees
+-- ORDER BY first_name DESC
+-- 		DESC for descending order 
+--  	else it will be arranged in assending order
+ORDER BY office_id DESC , first_name
---
+++ hacktoberfest 2022/Sql/sorting2.sql
@@ -0,0 +1,4 @@
+USE sql_hr;
+SELECT first_name, last_name, job_title FROM employees
+ORDER BY 3 , 1
+-- through this sorting the table with 3 - job title & 1 - first name
